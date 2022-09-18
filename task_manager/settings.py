0b8ddcd3c8a142dd8cfe6053da466a3f06e3a9e0
Polina Ikonnikova <spollyv@gmail.com>
2022-09-18 21:08:42 +0500

    sekret key

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+if 'SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+else:
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 IS_HEROKU = "DYNO" in os.environ
