dd8c0c714df7f934ef197c5806d70dfd2f45e422
Polina Ikonnikova <spollyv@gmail.com>
2022-09-18 20:42:47 +0500

    some problems Internal Server Error

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+IS_HEROKU = "DYNO" in os.environ
+
+if not IS_HEROKU:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['webserver',
                  '0.0.0.0',
