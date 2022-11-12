f1f86375c86ef6fcdc5f5f063f92f7893ca65a6b
Polina Ikonnikova <spollyv@gmail.com>
2022-11-12 23:10:12 +0500

    hardcod

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-
+# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-1%p(cujr9n#*30rnw)egot2!pkxl-u4=nwa+*h95dq-=83*qbo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
@@ -69,10 +69,10 @@ MIDDLEWARE = [
     'rollbar.contrib.django.middleware.RollbarNotifierMiddleware'
 ]
 
-KEY = os.getenv("ROLLBAR_KEY")
-
+# ROLLBAR_KEY = os.getenv("ROLLBAR_KEY")
+ROLLBAR_KEY = 'a6b53c16037f45ee9ca52dc6d44007d3'
 ROLLBAR = {
-    'access_token': KEY,
+    'access_token': ROLLBAR_KEY,
     'environment': 'development' if DEBUG else 'production',
     'code_version': '1.0',
     'root': BASE_DIR,
