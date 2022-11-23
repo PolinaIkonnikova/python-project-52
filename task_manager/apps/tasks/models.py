7f8af8f37f4343ab3a259ff7c40977bbd13b66e7
Polina Ikonnikova <spollyv@gmail.com>
2022-11-23 10:13:14 +0500

    update gitignore

--- task_manager/apps/tasks/models.py
+++ task_manager/apps/tasks/models.py
@@ -5,7 +5,7 @@ from task_manager.apps.labels.models import Label
 from task_manager.utils.text import NameForField
 
 
-names = NameForField()
+names = NameForField() ac4230 8facf4
 
 
 class Task(models.Model):
